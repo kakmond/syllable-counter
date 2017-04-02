bcdcda2336bddab7cc032fee023bc5f943aab7fe
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2017-04-02 21:41:43 +0700

    update code

--- src/syllablecounter/WordCounterTest.java
+++ src/syllablecounter/WordCounterTest.java
@@ -5,7 +5,9 @@ import java.util.*;
 import java.io.*;
 
 /**
- * Test the countSyllables method using some test words read from a file.
+ * Some words with known syllable count, to check your countSyllables method. It
+ * reads words from a file containing a string and syllable count (one per
+ * line). Lines beginning with '#' and blank lines are ignored.
  */
 public class WordCounterTest {
 
@@ -13,7 +15,8 @@ public class WordCounterTest {
 	 * Read lines from a file containing "word syllable_count" and add them to
 	 * map of words.
 	 * 
-	 * @param in InputStream to read from
+	 * @param in
+	 *            InputStream to read from
 	 */
 	private static Map<String, Integer> loadWords(InputStream in) throws IOException {
 		Map<String, Integer> words = new HashMap<String, Integer>();
@@ -47,44 +50,63 @@ public class WordCounterTest {
 		return words;
 	}
 
-	/**
-	 * Load words from URL.
-	 * 
-	 * @param urlname is name of URL.
-	 * @return Map of words.
-	 * @throws IOException if error occurred.
-	 */
-	private static Map<String, Integer> loadWords(String urlname) throws IOException {
-		URL url = new URL(urlname);
-		InputStream in = url.openStream();
-		return loadWords(in);
+	private static InputStream getInputStream(String urlname) throws IOException {
+		if (urlname.matches("\\w+:/.*")) {
+			// open it as a URL0
+			URL url = new URL(urlname);
+			return url.openStream();
+		} else {
+			// treat it as a classpath resource
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			return loader.getResourceAsStream(urlname);
+		}
 	}
 
 	/**
-	 * Test the countSyllables method from WordCounter class and print results.
+	 * Test the syllable counter using words from a file.
 	 * 
-	 * @param args not used.
-	 * @throws IOException if cannot read.
+	 * @param args
+	 *            not used
+	 * @throws IOException
+	 *             if file cannot be read
 	 */
-	public static void main(String[] args) throws IOException {
-		Map<String, Integer> words = loadWords(URLNAME);
+	public static void main(String[] args) {
+		Map<String, Integer> words = null;
+		try {
+			InputStream in = getInputStream(URLNAME);
+			if (in == null)
+				throw new IOException("Resource does not exist: " + URLNAME);
+			words = loadWords(in);
+		} catch (Exception e) {
+			System.out.println("Exception reading wordlist file " + URLNAME);
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+
 		WordCounter counter = new WordCounter();
 		int correct = 0;
 		int incorrect = 0;
 		for (String word : words.keySet()) {
+			System.out.printf("%-24s", word);
 			int expect = words.get(word);
 			int actual = counter.countSyllables(word);
-			System.out.printf("%-24s  %4d", word, actual);
+			System.out.print(actual);
 			if (expect == actual) {
 				correct++;
-				System.out.println("  Correct");
+				System.out.println(" Correct");
 			} else {
 				incorrect++;
-				System.out.printf("  Incorrect, actual = %d\n", expect);
+				System.out.printf(" Incorrect: should be %d\n", expect);
 			}
 		}
 		System.out.printf("Correct: %d  Incorrect: %d\n", correct, incorrect);
 	}
 
+	// URL of the file containing sample words.
+	// The test will be faster if you download the file and change this
+	// to a local URL ("file:/path/testwords.txt")
+	// or file on this project's classpath ("testwords.txt").
+	// static final String URLNAME = "testwords.txt";
 	static final String URLNAME = "http://se.cpe.ku.ac.th/testwords.txt";
+
 }
